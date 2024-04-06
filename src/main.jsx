d129a48365d8aaf463ea8b5d2f0373c5c79dd2ff
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-06 13:44:04 +1100

    formats document

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
 
-const router =createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App />
+    path: '/',
+    element: <App />
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
