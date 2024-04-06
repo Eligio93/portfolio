3a1f7516719ebfd60447663290d23fac0cbe80df
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-06 13:43:04 +1100

    Init Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link, NavLink } from "react-router-dom"
+
+export default function Header() {
+    return (
+    <header className="header">
+        <nav>
+            <ul>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/projects'>Projects</NavLink></li>
+                <li><NavLink to='/contacts'>Contacts</NavLink></li>
+            </ul>
+        </nav>
+
+    </header>)
+
+
+}
