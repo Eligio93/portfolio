c9673cb6ac8a794453b10044a71cbc8c8e7ff99e
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-07 11:20:10 +1000

    init hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero(){
+    return <div className="hero">
+        <p>Welcome to:</p>
+        <h1>EC Web Developer</h1>
+        <p>Web Development made easy</p>
+    </div>
+    
+}
