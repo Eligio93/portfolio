73f09d7e3320c37718a0d3bb18eeb25474b519c5
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-07 11:21:00 +1000

    adds new routes to router

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Projects from './components/Projects'
+import Contacts from './components/Contacts'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />
+  },
+  {
+    path:'/projects',
+    element:<Projects />
+  },
+  {
+    path:'/contacts',
+    element: <Contacts />
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
