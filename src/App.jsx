1a1bed7fa2e4257c7a1cc09be408ad066adfba31
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-08 15:24:05 +1000

    init technologies section

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,74 @@ import Hero from "./components/Hero";
 import SpecCard from "./components/SpecCard";
 import frontEndIcon from './assets/frontendIcon.svg'
 import mobileIcon from './assets/mobileIcon.svg'
+import javascriptIcon from './assets/javascriptIcon.svg'
+import cssIcon from './assets/cssIcon.svg'
+import gitHubIcon from './assets/gitHubIcon.svg'
+import htmlIcon from './assets/htmlIcon.svg'
+import nodeIcon from './assets/nodeIcon.svg'
+import reactIcon from './assets/reactIcon.svg'
+import viteIcon from './assets/viteIcon.svg'
+import vsIcon from './assets/vsIcon.svg'
+import webpackIcon from './assets/webpackIcon.svg'
 import "./style.css"
 
+let technologies = [
+  {
+    name: 'Javascript',
+    icon: javascriptIcon
+  },
+  {
+    name: 'CSS',
+    icon: cssIcon
+  },
+  {
+    name: 'GitHub',
+    icon: gitHubIcon
+  },
+  {
+    name: 'HTML',
+    icon: htmlIcon
+  },
+  {
+    name: 'Node',
+    icon: nodeIcon
+  },
+  {
+    name: 'React',
+    icon: reactIcon
+  },
+  {
+    name: 'Vite',
+    icon: viteIcon
+  },
+  {
+    name: 'Visual Studio',
+    icon: vsIcon
+  },
+  {
+    name: 'WebPack',
+    icon: webpackIcon
+  }
+
+]
+
+
 function App() {
-
-
   return (
     <>
       <Header />
       <Hero />
+      <section className="used-technologies">
+        <h2>Used Technologies</h2>
+        <div className="technologies-container">
+          {technologies.map((technologie) => {
+            return <div className="technology" key={technologie.name}>
+              <img src={technologie.icon} alt={technologie.name + '_icon'} />
+              <p>{technologie.name}</p>
+            </div>
+          })}
+        </div>
+      </section>
 
       <section className="areas-specialization">
         <h2>Areas of Interest</h2>
@@ -25,8 +84,8 @@ function App() {
             title={"Mobile Development"}
           />
         </div>
-
       </section>
+
     </>
   )
 }
