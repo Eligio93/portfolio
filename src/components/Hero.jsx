671200ed767d3e9778d424286bbd3b0716da3a78
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-08 17:17:07 +1000

    adds buttons to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom"
+
+
 export default function Hero(){
     return <div className="hero">
+        <div className="hero-description">
         <p>Welcome to:</p>
         <h1>EC Web Developer</h1>
         <p>Web Development made easy</p>
+        </div>
+        <div className="hero-buttons">
+        <Link to='/projects'><button>Explore Portfolio</button></Link>
+        <button>Get in touch</button>
+        </div>
+
     </div>
     
 }
