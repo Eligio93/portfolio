327839a1984c18957e5322ebc9185b472b0ae788
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-08 17:16:21 +1000

    passes description as prop

--- src/components/SpecCard.jsx
+++ src/components/SpecCard.jsx
@@ -1,7 +1,7 @@
-export default function SpecCard({icon,title}){
+export default function SpecCard({icon,title,description}){
     return <div className="spec-card">
             <img src={icon} alt="front-end Icon" />
             <p>{title}</p>
-            <p>La descrizione del prodotto espressa gia da qui</p>
+            <p>{description}</p>
     </div>
 }
