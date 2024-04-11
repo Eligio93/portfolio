1479ea0d08e1fba9fb8dc83f8a4cdac10b561126
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-12 09:47:34 +1000

    restore file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import projectsList from '../../public/projects.json'
+import projectsList from '../projects.json'
 import { v4 as uuidv4 } from 'uuid';
 import gitIcon from "../assets/gitIconFooter.svg"
 import liveIcon from "../assets/liveIcon.svg"
