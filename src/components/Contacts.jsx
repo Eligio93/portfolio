6558a607bf47ca4e32877e3c0794cc04e14448b6
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-11 13:19:53 +1000

    finalises structure of Contacts page

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,3 +1,41 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import linkedinIcon from '../assets/linkedinIcon.svg';
+import gitHubIcon from '../assets/gitIconFooter.svg'
+import mailIcon from '../assets/emailIcon.svg'
+
+
 export default function Contacts(){
-    return <h1>Contacts</h1>
+    return (
+        <>
+        <Header />
+        <section className="contacts">
+            <div className="contact-description">
+                <p>Get</p>
+                <p>In</p>
+                <p>Touch</p>
+            </div>
+           <div className="contact-social">
+            <div className="contact-linkedin">
+                <img src={linkedinIcon} alt="linkedin-icon" />
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/eligio-cristantielli">Linkedin</a>
+
+            </div>
+            <div className="contact-git">
+                <img src={gitHubIcon} alt="gitHub-icon" />
+                <a href="https://github.com/Eligio93" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </div>
+            <div className="contact-mail">
+                <img src={mailIcon} alt="email-icon" />
+                <a href="mailto: eligio.cristantielli@gmail.com">eligio.cristantielli@gmail.com</a>
+            </div>
+           
+           </div>
+          
+        </section>
+        <Footer/>
+       
+        </>
+
+    )
 }
